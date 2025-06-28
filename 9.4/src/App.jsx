b3a3f8d923874bf7fa78e7444c20e5edb3f06d1e
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-06-28 08:47:47 +0530

    learned about the childern prop

--- 9.4/src/App.jsx
+++ 9.4/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// this is  childer prop highly used as wrapper prop like if we have to put something in header and footer
+import { Children } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+  return <div style={{ display: "flex" }}>
+    <Card>
+      <div style={{ backgroundColor: "red" }}>
+        <h2>
+          Card Title
+        </h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is some content inside the card
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Card>
+    <Card>
+      <div style={{ backgroundColor: "blue" }}>
+        <h2>
+         Another Card Title
+        </h2>
+        <p>
+          This is some content inside the card
+        </p>
+      </div>
+    </Card>
+  </div>
+}
+
+function Card({ children }) {
+  return <div style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '20px', margin: '10px' }}>
+    {children}
+  </div>
 }
 
 export default App
+
+// 
