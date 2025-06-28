d79802397d43b59fb1898d405cc306d40c8e1590
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-06-28 13:28:29 +0530

    learned layout and path rendering

--- 10.1/src/App.jsx
+++ 10.1/src/App.jsx
@@ -1,44 +1,79 @@
 // this is the simple logic of the routing
-import { BrowserRouter, Routes, Route , Link, useNavigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link, useNavigate , Outlet } from "react-router-dom"
 
 function App() {
   return <div>
     <BrowserRouter>
-    <Link to={"/"}>PW</Link>
-    <Link to={"/neet/online-coaching-class-11"}>11 Neet</Link>
-    <Link to={"/neet/online-coaching-class-12"}>12 Neet</Link>
       <Routes>
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+          <Route index element={<Landing />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
       </Routes>
+      Footer | Contact Us
     </BrowserRouter>
   </div>
 }
 
-function Landing(){
+
+function Layout() {
+  return <div style={{ height: "100vh" }}>
+    <Header></Header>
+    <div style={{ height: "90vh" }}>
+      <Outlet />
+    </div>
+    footer
+  </div>
+}
+
+
+function Header() {
   return <div>
-      welcome to pw
+
+    <Link to={"/"}>PW</Link>
+    <Link to={"/neet/online-coaching-class-11"}>11 Neet</Link>
+    <Link to={"/neet/online-coaching-class-12"}>12 Neet</Link>
+
+  </div>
+}
+
+
+function Landing() {
+  return <div>
+    welcome to pw
   </div>
 }
 
 
 
-function Class11Program(){
+function Class11Program() {
   return <div>
     Neet programs for class 11th
   </div>
 }
-function Class12Program(){
- const navigate = useNavigate();
 
- function redirecting(){
-  navigate("/")
- }
+
+
+
+function Class12Program() {
+  const navigate = useNavigate();
+
+  function redirecting() {
+    navigate("/")
+  }
 
   return <div>
     Neet programs for class 12th
     <button onClick={redirecting}>Go back to the landing page</button>
+  </div>
+}
+
+
+function ErrorPage() {
+  return <div>
+    No page is found
   </div>
 }
 
