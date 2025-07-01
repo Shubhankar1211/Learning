fd74f638f0d563c2410d59c5c0ab77b56443fbf3
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-07-01 09:08:06 +0530

    learned about the rolling state

--- 10.2/src/App.jsx
+++ 10.2/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// this is the consept of rooling ->: if a child want to access the state property of its sibling it cant access it to acces it we use the  concept of rolliong the state
+// in rolling the state we then pass the component to the least common ansestar so that its child can access the state 
+
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <LightBulb></LightBulb>
     </>
   )
 }
+
+
+function LightBulb() {
+  const [bulbon, setbulbon] = useState(true)
+  return <div>
+    <BulbState bulbon={bulbon}></BulbState>
+    <ToggleBulbState bulbon={bulbon} setbulbon={setbulbon}></ToggleBulbState>
+  </div>
+}
+
+
+function BulbState({bulbon}) {
+
+
+  return <div>
+    {bulbon ? "Bulb On" : "Bulb Off"}
+  </div>
+}
+
+
+
+function ToggleBulbState({bulbon,setbulbon}) {
+  function ToggleButton() {
+    setbulbon(!bulbon)
+  }
+  return <div>
+    <button onClick={ToggleButton}>Toggle the bulb</button>
+  </div>
+}
+
 
 export default App
