1e20d76a6a860e7e4e76636b77b093a760e09de4
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-07-10 19:14:07 +0530

    learned about the context api

--- 10.2/src/App.jsx
+++ 10.2/src/App.jsx
@@ -1,9 +1,9 @@
 // this is the consept of rooling ->: if a child want to access the state property of its sibling it cant access it to acces it we use the  concept of rolliong the state
 // in rolling the state we then pass the component to the least common ansestar so that its child can access the state 
 
-import { useState } from "react"
 
 
+/*
 function App() {
   return (
     <>
@@ -33,6 +33,67 @@ function BulbState({bulbon}) {
 
 
 function ToggleBulbState({bulbon,setbulbon}) {
+  function ToggleButton() {
+    setbulbon(!bulbon)
+  }
+  return <div>
+    <button onClick={ToggleButton}>Toggle the bulb</button>
+  </div>
+}
+  */
+
+
+
+
+
+// this is how we deal with the prop drilling problem by using context api
+
+import { createContext, useContext, useState } from "react"
+const BulbContext = createContext(); // this is how we declare the context api 
+
+// this is how we use context api by wrapping inside them
+// this is our custom bulb provider 
+function BulbProvider({ children }) {
+  const [bulbon, setbulbon] = useState(true);
+    return < BulbContext.Provider value = {{
+      bulbon: bulbon,
+      setbulbon: setbulbon
+    }}>
+  { children }
+      </BulbContext.Provider >
+}
+
+
+function App() {
+
+  return (
+    <div>
+      <BulbProvider>
+        <Light></Light>
+      </BulbProvider>
+    </div>
+  )
+}
+
+
+function Light() {
+  return <div>
+    <LightBulb></LightBulb>
+    <LightSwitch></LightSwitch>
+  </div>
+}
+
+
+function LightBulb() {
+  const { bulbon } = useContext(BulbContext);
+  return <div>
+    {bulbon ? "Bulb On" : "Bulb Off"}
+  </div>
+}
+
+
+function LightSwitch() {
+  const { bulbon, setbulbon } = useContext(BulbContext);
   function ToggleButton() {
     setbulbon(!bulbon)
   }
--- 10.2/src/notes.js
+++ 10.2/src/notes.js
@@ -17,16 +17,13 @@ to fix the problem of prop drilling
  
 we are  using conpcept of context api
 the context api is a powerful  feature in react that enables you to manages state across your application more effectively, especially when dealing with deeply nested somponets
-without having to pass props down amnually at every level
+the context api provides a way to share values(state , functions , etc) between components without having to pass props down amnually at every level
 
 The Context API is a feature in React that allows global state management by providing a way to pass data deeply through the component tree without having to pass props manually at every level.
-
 In simple words:
-
 Context lets you “broadcast” data to many components without prop drilling.
 
 Before Context, we had to "prop drill" — pass data from a parent to a deeply nested child by forwarding it through each intermediate component, even if those intermediate components didn’t need the data.
-
 This made the code less maintainable and more complex.
 The Context API solves this problem by allowing components to subscribe to global data.
 */
