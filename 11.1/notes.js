a8e591ac0d1589a7f128041c5fb898d561fe519c
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-07-11 11:55:37 +0530

    learned about the basic if custom hooks

---
+++ 11.1/notes.js
@@ -0,0 +1,31 @@
+/*
+useState=> store a state variable ,re-render a component when the variable changes
+useEffect=> Run an effect whenever a depedency changes
+CustomHooks
+
+this is a custom hook beacuse we are using use and if we have to make the custom hook we have to use the use key words
+
+function useDebounce(){
+cosnt[value,setvalue]=useSate()
+return value
+}
+
+
+Custom hooks in react are a powerful festure that allows you to encapsulate and reuse stateful logic across differnet compontes 
+they are esentially js functions that can use react hooks internally . by creating custom hooks, you can abstaract away complex logic , making ypur componetns cleaner and more manageable.
+
+why use custom hooks 
+Resussebility : if you find yourself using the smae logic in multiple components , a custom hook can help you avoid code duplication.
+Seperation of concenrs : they allow you to seperate business logic from ui logic , making ypur components more focused and eseir to understand
+
+common custom hooks -:
+usefetch
+usefetch eith re-fetching
+useprov
+useisonline
+usedebounce
+
+
+
+
+*/
--- 11.1/src/App.jsx
+++ 11.1/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// here are custom hooks learning
 
+import { useState } from "react"
+
+// this is the simple version of are application
+/*
+import { useState } from "react"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        <Counter></Counter>
+    </div>
   )
+}
+
+function Counter(){
+  const [count , setcount ]= useState(0);
+
+  function increase(){
+    setcount(c=>c+1);
+  }
+  return<div>
+     <button onClick={increase}>start {count}</button>
+  </div>
+}
+export default App
+*/
+
+// here we are making this with the help of the custom hooks 
+function useCounter(){
+  const [count , setcount] = useState(0);
+
+  function increase(){
+    setcount(count+1);
+  }
+  return {
+     count : count,
+     increase: increase
+  }
+}
+
+function App(){
+  const {count , increase} = useCounter(); // this is destructuring the object
+  return (<div>
+     <button onClick={increase}> Count {count}</button>
+  </div>)
 }
 
 export default App
