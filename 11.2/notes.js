cc07a1f62526a0f35ac93b63d8fc1f58e609ee48
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-07-15 09:00:44 +0530

    learned about the recoil how it is differnet from the context api

---
+++ 11.2/notes.js
@@ -0,0 +1,9 @@
+/*
+a state management library for react that provides a way to manage global state with fine- grained control
+recoil minimizes unnecessary rneders by only re-rendering components that depends on changed atoms 
+the performance of a react app is measured by the number of re-renders. Each re- render is  expensive , you should aim to minimise it
+
+key concepts in recoil 
+atom: unit of state that can be read from and written to from any component
+selectors : functions that derive state from stoms or other selectors, allowing for computed state
+*/ 
--- 11.2/src/App.jsx
+++ 11.2/src/App.jsx
@@ -1,9 +1,68 @@
+// learning recoil
+import { useState } from "react"
+
 function App(){
   return <>
-  <div></div>
+  <div>
+    <Counter1></Counter1>
+  </div>
   </>
 }
 
+/*
+this is the simple application with the help of use state  
+function Counter(){
+const[ count , setcount]  =useState(0);
+function IncreaseCount(){
+     setcount(count+1);
 
+}
+
+function DecreaseCount(){
+  if(count>0){
+    setcount(count-1);
+  }
+}
+
+  return <div>
+    <div>{count}</div>
+    <button onClick={IncreaseCount}>Increase</button>
+    <button onClick={DecreaseCount}>Decrease</button>
+  </div>
+}
 
 export default App
+*/
+
+
+// it is row state varibale by passing props
+
+function Counter1(){
+  const [count ,setcount] =useState(0);
+  return <div>
+    {count}
+   <IncreaseCount setcount ={setcount}></IncreaseCount>
+   <DecreaseCount setcount ={setcount}></DecreaseCount>
+  </div>
+}
+
+function IncreaseCount({setcount}){
+  function Increase(){
+    setcount(count=>count+1)
+  }
+  return <div>
+    <button onClick={Increase}>Increase</button>
+  </div>
+}
+
+function DecreaseCount({setcount}){
+  function Decrease(){
+    setcount(count=>count-1)
+  }
+  return <div>
+  <button onClick={Decrease}>Decrease</button>
+  </div>
+}
+
+export default App
+
