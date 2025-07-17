e4ffaa6e17154f944e91e14128753ad8254e17d2
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-07-17 20:35:28 +0530

    learning about ui ux

---
+++ 12.1/notes.js
@@ -0,0 +1,102 @@
+/*
+//typography
+
+typeface is the designe of the letter(ex , poppins),font refers to a specific style or weight within that typeface
+in simple terms typeface is the family , and font is the individual member within the family
+
+
+
+150 for heading 
+and 100- 120 for ReportBody
+
+
+latter spacing shoul dbe form -1 to 3%
+these are the rresourse for the typography
+leran form demo webside like typescale , fontshare , goggle fonsts, font joys(font piars ke liye  best h ) , freefaces ,uncut.wtf
+atr the end of the day the text in you design should be legible amd readable
+
+
+// colors
+what is ths color theroy 
+it is the study of how colors interact and the effect the crerate when  combined . parts if color theory are as follows:
+1. Color harmony : the use of colors in a way that creates a visiually pleasing and balanced compostions
+2. color value : refers to the releative lightness, darkness and saturation and hue.
+3  color temperature: it is used to describe how cool or warn the color is . warm colors(such as red , roange , yellow) are associated with felling of warmth, excitement and energy. cool colors(like blue , green and purple) are often linked to felling of calm . relaxation , and serenity.
+
+contrast -:
+
+It refers to the differnce in luminacne or color that makes an object distinguishable form others. Good contrast improves readability and ensures that key elemnets stand out.
+
+60 30 10 rule 
+60 is neutral color (dominant color - this will be hte main color of your interface)(backgorund or large section)
+30 secondary color (secondary color) - used to add contrast to the dominat color(side bars, cards ,button)
+10 main brand color(signin fying the brand) use =d sparingly ti hieghtligh important actions(cta button , icons , links)
+
+the problems with  too many colors
+overwhelming the user
+brand dilution
+lack of hierarchy
+common mistakes
+
+resorces -:
+ 
+very good websites-: Realtimecolores(test some color grading or testing)+
+uicolors.app -: (good for tailwind css)
+luemint ( htese are color pallet generator);
+
+
+
+// ui/ ux
+ ui is teh which we seeon the wb page aand ux is which we fell on the web page
+
+ ux some roles -:
+
+ hick's law 
+ prinicple -: the more choices oyu present the longer it teake for users to make a 
+ application -: lomit choices and break complex tasks inot smaller steps(multi - steps -forms)
+
+ fitts law -: the tiem to reach a target depends on its size and distance
+ applications : make buttons large and place frequently used elements within easy reach especially on mobile
+
+
+
+ jacob laws-:peple prefer interfaces that work lime those the have already familiar with 
+ applications : stick to established Ui patterns 
+
+
+miller'law -: pepple cna hold 7(+-2) itmes in thier working memoery
+applications : check infirmation and avoids overwhelming users with too many details at once
+
+law of proximity - : objests close to each other are perceived as realted
+applications : gorup relatedd elemts together 
+
+asthetic - usability effect -: users perceive visually appealing desingns as more unsable
+applications prioritixe both asthetics and fucntiaonality to improve user satifaction1-
+
+
+lummi.ai  is good for getting pics
+hygen ai video genterator
+dribble.com
+bento gird 
+
+
+
+
+her are some webs which we uses for refernces
+ultre - dark themed websites
+freelance things
+emil kowasiki
+animations on the web
+vishnu prasad vp - Digital Product Designer
+konstantin minister - product engineer
+google design
+dark themed web designe inspriration
+design tools 
+dark mode designes
+minimal gallery - websites design inspiration
+design treasure
+refero
+awwards
+Material designe
+60fps
+*/
