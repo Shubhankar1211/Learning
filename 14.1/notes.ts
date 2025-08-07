9e5e739fbca9b2b2004c55dc47e0a430552b34c7
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-08-07 19:38:14 +0530

    fixing bugs

--- 14.1/notes.ts
+++ 14.1/notes.ts
@@ -11,6 +11,7 @@ target -: the target option in a tsconfig file specifices the ecma script target
 rootdir -: where should the compiler look for ts files. good practise is for this to be the src folder
 otdir -: where should the complier look for spit out the js files
 noimplicityAny -:
+
 removeComments -: weather or not to include commnets in the final js file
 
 */
