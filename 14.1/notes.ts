1e51f1ed98390f85f5b43d824015da7166394b15
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-08-23 19:35:37 +0530

    learning new things

--- 14.1/notes.ts
+++ 14.1/notes.ts
@@ -7,6 +7,7 @@ npx convert it into js
 
 some tsconfig things that we have to alsways add in our files
 
+
 target -: the target option in a tsconfig file specifices the ecma script target version to which the ts compiler will complie the ts code
 rootdir -: where should the compiler look for ts files. good practise is for this to be the src folder
 otdir -: where should the complier look for spit out the js files
