5ffa1f188b5dcd50056e0434a96ed8834eccec1e
Shubhankar Chauhan <shubhankarchauhan09@gmail.com>
2025-10-23 21:59:12 +0530

    qa

--- 14.2/notes.ts
+++ 14.2/notes.ts
@@ -197,6 +197,7 @@ This defines a Person class with a property, a constructor, and a method.
 
 
 
+
 How do access modifiers (public, private, protected) work in TypeScript classes?
 public (default): Accessible anywhere.
 
